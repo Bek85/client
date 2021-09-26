0cbe6fcddfa0a4431717ea01c65c1253d932e021
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-27 03:41:07 +0500

    attach callback functions to buttons to initiate auth2 sign in and sign out process

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,19 +25,28 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red icon google button">
-          <i className="google icon"></i>Sign Out
+        <button className="ui red icon google button" onClick={this.onSignOut}>
+          <i className="google icon" />
+          Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
-          <i className="google icon"></i>
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
           Sign In with Google
         </button>
       );
