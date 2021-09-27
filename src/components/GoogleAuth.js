d47dea0e6d543cfef513c3432064d0f9c22ed37f
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-27 14:37:08 +0500

    add redux, react-redux & initial redux setup

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,11 +25,11 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -38,14 +38,17 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red icon google button" onClick={this.onSignOut}>
+        <button
+          className="ui red icon google button"
+          onClick={this.onSignOutClick}
+        >
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button" onClick={this.onSignIn}>
+        <button className="ui red google button" onClick={this.onSignInClick}>
           <i className="google icon" />
           Sign In with Google
         </button>
