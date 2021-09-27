70d85401eb9a1d1c7a076f95e75383f7abe3ef79
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-27 15:39:00 +0500

    get the signed in user id and store it redux store

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,15 +2,16 @@ import { SIGN_IN, SIGN_OUT } from '../actions/actionTypes';
 
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null,
 };
 
 export default function authReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payload };
 
     case SIGN_OUT:
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userId: null };
 
     default:
       return state;
