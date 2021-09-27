b198750d75f5b43d8ccc008672e20cd618d77e5c
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-09-27 14:51:11 +0500

    build an authReducer to hook it up to redux

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = {
+  isSignedIn: null,
+};
+
+export default function authReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return { ...state, isSignedIn: true };
+
+    case 'SIGN_OUT':
+      return { ...state, isSignedIn: false };
+
+    default:
+      return state;
+  }
+}
