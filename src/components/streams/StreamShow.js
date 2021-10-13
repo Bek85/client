736e010b02ac7dcf870c284626ec40ebed56f6da
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-13 12:26:57 +0500

    build player after the streams have been fetched

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -12,10 +12,22 @@ class StreamShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
 
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: 'flv',
-      url: `http://localhost:8000/live/${id}`,
+      url: `http://localhost:8000/live/${id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
